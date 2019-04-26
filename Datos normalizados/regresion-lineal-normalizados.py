337f2b06f42309d2fa14e949519e5f20a5190ca1
Jhon Baron <jhooomn@gmail.com>
2019-04-26 15:34:24 -0500

    Normalizados y No-Normalizados
    - Datos normalizados
    - Datos no-normalizados
    - Graficas con datos normalizados y no-normalizados

---
+++ Datos normalizados/regresion-lineal-normalizados.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 15:12:45 2019
+
+@author: Jhon Baron
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+from sklearn import preprocessing
+
+data = pd.read_csv('movies.csv', header=0);
+#print(data)
+df = pd.DataFrame(data);
+#print(df)
+
+scaler = preprocessing.Normalizer(norm='l2', copy=True)
+df[['imdb_score','movie_facebook_likes']]=scaler.fit_transform(df[['imdb_score','movie_facebook_likes']])
+
+score = df['imdb_score'];
+likes= df['movie_facebook_likes'];
+#print(score) #x
+#print(likes) #y
+
+n=5043; #No. datos/filas
+sumx = sum(score);
+sumy = sum(likes);
+sumx2 = sum(score*score);
+sumy2 = sum(likes*likes);
+sumxy = sum(score*likes);
+promx = sumx/n; #Promedio de score
+promy = sumy/n; #Promedio de likes
+
+m = (sumx*sumy - n*sumxy)/(sumx**2 - n*sumx2);#Pendiente
+b = promy - m*promx;#Intercepto
+
+sigmax = math.sqrt(sumx2/n - promx**2); 
+sigmay = math.sqrt(sumy2/n - promy**2);
+sigmaxy = sumxy/n - promx*promy;
+
+R2 = (sigmaxy/(sigmax*sigmay))**2; #Coeficiente de correlacion
+
+print('Coeficiente de correlacion cuadrado:', R2);
+print('Pendiente: ', m);
+print('Intercepto: ', b);
+
+plt.plot(score,likes,'o', label = 'Datos');
+plt.plot(score, m*score + b, label = 'Ajuste');
+plt.ylabel('Likes');
+plt.xlabel('Score');
+plt.title('Regresion lineal');
+plt.grid();
+plt.legend();
